a2954c8880f99247fc8a2c28ffeabeab90129c6e
JOFLIX <joflixooko@outlook.com>
2019-09-05 12:14:19 +0300

    created a django api with both login and logout

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/moringa-school/Desktop/DJANGO_RESTFUL_API/tutorial/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
